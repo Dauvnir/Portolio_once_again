38c7e789ca7e6e341dda48fdf4deedac62fd2a1f
Dauvnir <pdawidpolak@gmail.com>
2025-08-02 17:48:42 +0200

    Added setting bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import emailjs from "@emailjs/browser";
 import { ThemeProvider } from "styled-components";
-import AnimateSections from "./components/AnimateSections";
+// import AnimateSections from "./components/AnimateSections";
+import SettingBar from "./mainComponents/SettingBar";
+import { darkTheme, lightTheme } from "./theme/theme";
 
 function App() {
 	const [isDarkMode, setDarkMode] = useState(false);
 	const toggleDarkMode = (e) => {
 		setDarkMode(e);
+	};
+
+	const [isEngLang, setEngLang] = useState(true);
+	const toggleLang = () => {
+		setEngLang(e);
 	};
 
 	useEffect(() => {
@@ -19,7 +26,11 @@ function App() {
 		emailjs.init(import.meta.env.VITE_PUBLIC_KEY);
 	}, []);
 
-	return <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}></ThemeProvider>;
+	return (
+		<ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+			<SettingBar toggle={toggleDarkMode} darkMode={isDarkMode} toggleLang={toggleLang} engLang={isEngLang} />
+		</ThemeProvider>
+	);
 }
 
 export default App;
---
+++ src/mainComponents/SettingBar.jsx
@@ -0,0 +1,61 @@
+import styled, { useTheme } from "styled-components";
+
+const Container = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	flex-direction: row;
+
+	position: sticky;
+	top: 0;
+	left: 0;
+
+	width: 100%;
+	height: 4rem; //to change
+
+	padding: 0.5rem; // to change
+
+	background-color: ${({ theme }) => theme.backgroundColor};
+
+	button {
+		height: 100%;
+		width: auto;
+
+		border: none;
+		background: none;
+
+		cursor: pointer;
+		img {
+			height: 100%;
+			width: 100%;
+
+			object-fit: contain;
+		}
+
+		span {
+			color: ${({ theme }) => theme.fontColor};
+		}
+	}
+`;
+
+const SettingBar = ({ toggle, darkMode, toggleLang, engLang }) => {
+	const theme = useTheme();
+	const handleChangeTheme = () => {
+		toggle(!darkMode);
+	};
+	const handleChangeLang = () => {
+		toggleLang(!engLang);
+	};
+	return (
+		<Container>
+			<button onClick={handleChangeTheme}>
+				<img src={theme.themeIcon} alt="toggle_theme" />
+			</button>
+			<button onClick={handleChangeLang}>
+				<span>PL | EN</span>
+			</button>
+		</Container>
+	);
+};
+
+export default SettingBar;
